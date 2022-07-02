be5ccd8e4581c88012a5a6391ed412e11d217824
GeonyoungPark <deanparkgy@gmail.com>
2022-07-02 13:17:43 +0900

    test

--- src/main/java/kr/co/goc/anumal/Main.java
+++ src/main/java/kr/co/goc/anumal/Main.java
@@ -5,5 +5,6 @@ import kr.co.goc.anumal.animalController.AniamlController;
 public class Main {
 	public static void main(String[] args) {
 		new AniamlController();
+		System.out.println("hi");
 	}
 }
